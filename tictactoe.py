4fad85fb0d84b6fdd97e2c23364995667b8ca91f
Daniel Thwaites <danthwaites30@btinternet.com>
2020-03-22 12:45:42 +0000

    feat: add map of squares to numbers
    Display numbers for each square at the start of the game, to assist with input.

--- tictactoe.py
+++ tictactoe.py
@@ -7,6 +7,20 @@ def print_board(board):
     print('|', board[6], '|', board[7], '|', board[8], '|')
     print('----|---|----')
 
+def print_map():
+    print('----|---|----')
+    print('| 1 | 2 | 3 |')
+    print('----|---|----')
+    print('| 4 | 5 | 6 |')
+    print('----|---|----')
+    print('| 7 | 8 | 9 |')
+    print('----|---|----')
 
-board = 'X' * 9
-print_board(board)
+
+# The board is stored as a string of 9 letters. This would look something
+# like "XXXOOOXXX". The first 3 form the first row, the next 3 are the second
+# row and the final 3 are the last row.
+board = ' ' * 9
+
+# Show the players which number corresponds to which square
+print_map()
